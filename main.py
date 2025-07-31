f7afcaebb0535f39da9bf30763b8bd23310b5634
a1234 <readerknowing@gmail.com>
2025-07-31 06:06:50 +0500

    add

--- main.py
+++ main.py
@@ -9,13 +9,42 @@ bot = telebot.TeleBot(self)
 #  Ответ бота на команды старт
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+    welcome_text = (
+        "👋 Добро пожаловать в *ZaimShield* 🛡\n\n"
+        "📌 *ВАЖНО*: Бот не является финансовой организацией и не выдаёт займы.\n"
+        "Он создан исключительно для помощи заемщикам:\n"
+        "— Разобраться в условиях микрозаймов\n"
+        "— Избежать скрытых комиссий и переплат\n"
+        "— Не платить за навязанные услуги и страховки или помощь вернуть выплату по страховке\n\n"
+        "📊 Мы даём информацию — выбор за вами."
+    )
+    bot.send_message(message.chat.id, welcome_text, parse_mode='Markdown')
+
+
+    text = (
+        "🙏 *Команда ZaimShield оформляет займы за собственные средства*, чтобы проверить:\n"
+        "— комиссии\n"
+        "— сроки обработки\n"
+        "— скрытые условия и возврат страховок\n\n"
+        "Если бот оказался полезен, вы можете *поддержать проект* — нажав кнопку ниже.\n\n"
+        "💸 Донат добровольный и ни к чему не обязывает, но нам будет приятно и важно понимать, что мы не зря стараемся.\n\n"
+        "Спасибо за вашу поддержку ❤️"
+    )
+    markup = types.InlineKeyboardMarkup()
+    donate_btn = types.InlineKeyboardButton("💖 Помощь в развитии проекта", url="https://your-donation-link.com")
+    markup.add(donate_btn)
+    bot.send_message(message.chat.id, text, reply_markup=markup, parse_mode='Markdown')
+
+
     markup = types.ReplyKeyboardMarkup()
     mkk = types.KeyboardButton('Выберите компанию')
     markup.row(mkk)
-    blank = types.KeyboardButton('Бланки')
-    adress = types.KeyboardButton('E-mail MKK')
-    markup.row(blank,adress)
+    feedback = types.KeyboardButton('Обратная связь')
+    markup.row(feedback)
+    donat = types.KeyboardButton('Помощь в развитии проекта')
+    markup.row(donat)
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}', reply_markup = markup)
+
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, '<b>Help information</b>', parse_mode='html')
